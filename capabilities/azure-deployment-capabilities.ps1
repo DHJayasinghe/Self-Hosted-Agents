5266400938148544b08dcfbbf24aaf2b0a90dbf5
Dhanuka Jayasinghe <hasitha2kandy@gmail.com>
2022-07-21 07:06:26 +0530

    Disabled switching back to DockerUser

--- capabilities/azure-deployment-capabilities.ps1
+++ capabilities/azure-deployment-capabilities.ps1
@@ -5,6 +5,9 @@ $packages = @(
   'azcopy10'
 )
 
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+Install-PackageProvider -Name NuGet
+
 foreach ($package in $packages) {
   Invoke-Expression -Command "choco install $package -y"
 }
