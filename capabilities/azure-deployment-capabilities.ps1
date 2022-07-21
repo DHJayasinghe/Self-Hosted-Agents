4423028dfab02eb540e8acba1dfb64b454bafa4d
Dhanuka Jayasinghe <hasitha2kandy@gmail.com>
2022-07-21 22:38:54 +0530

    Adding Execution Policy

--- capabilities/azure-deployment-capabilities.ps1
+++ capabilities/azure-deployment-capabilities.ps1
@@ -5,7 +5,10 @@ $packages = @(
   'azcopy10'
 )
 
+Set-PSRepository -Name 'PSGallery' -InstallationPolicy Trusted
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
 Install-PackageProvider -Name NuGet
 Install-Module -Name SqlServer -Force -Scope AllUsers
 
