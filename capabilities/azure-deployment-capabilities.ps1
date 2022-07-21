e1ceffc6e65563c192efa1733b8cb8631b052afb
Dhanuka Jayasinghe <hasitha2kandy@gmail.com>
2022-07-21 08:19:23 +0530

    Install SqlServer module

--- capabilities/azure-deployment-capabilities.ps1
+++ capabilities/azure-deployment-capabilities.ps1
@@ -7,6 +7,7 @@ $packages = @(
 
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 Install-PackageProvider -Name NuGet
+Install-Module -Name SqlServer -Force -Scope AllUsers
 
 foreach ($package in $packages) {
   Invoke-Expression -Command "choco install $package -y"
