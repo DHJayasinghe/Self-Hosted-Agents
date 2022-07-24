c30f7fb47d638249bdd64c933bd55f9d27250df2
Dhanuka Jayasinghe <hasitha2kandy@gmail.com>
2022-07-24 09:59:22 +0530

    Package provider install Nuget

--- capabilities/azure-deployment-capabilities.ps1
+++ capabilities/azure-deployment-capabilities.ps1
@@ -9,8 +9,7 @@ Set-PSRepository -Name 'PSGallery' -InstallationPolicy Trusted
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
-Install-PackageProvider -Name NuGet
-Install-Module -Name SqlServer -Force -Scope AllUsers
+Install-PackageProvider -Name NuGet -Force
 
 foreach ($package in $packages) {
   Invoke-Expression -Command "choco install $package -y"
