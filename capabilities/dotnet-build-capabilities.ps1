5a3b237ea27256cb8f7532fb355618bedc62dfd2
Dhanka Jayasinghe <DHJayasinghe@users.noreply.github.com>
2022-07-27 07:25:45 +0530

    Update dotnet-build-capabilities.ps1

--- capabilities/dotnet-build-capabilities.ps1
+++ capabilities/dotnet-build-capabilities.ps1
@@ -8,7 +8,7 @@ foreach ($package in $packages) {
 }
 
 # Delete everything in the dotnet folder that's not needed in the SDK layer but will instead be derived from base layers
-Get-ChildItem -Exclude 'LICENSE.txt', 'ThirdPartyNotices.txt', 'packs', 'sdk', 'sdk-manifests', 'templates', 'shared' -Path 'C:\Program Files\dotnet' `
-| Remove-Item -Force -Recurse; `
+# Get-ChildItem -Exclude 'LICENSE.txt', 'ThirdPartyNotices.txt', 'packs', 'sdk', 'sdk-manifests', 'templates', 'shared' -Path 'C:\Program Files\dotnet' `
+# | Remove-Item -Force -Recurse; `
   Get-ChildItem -Exclude 'Microsoft.WindowsDesktop.App' -Path 'C:\Program Files\dotnet\shared' `
 | Remove-Item -Force -Recurse
